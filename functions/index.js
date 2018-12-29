a45fe71dc9ffdda58ab9aac71307f22a2798a1b8
Robert Abreu <robertabreu@pursuit.org>
2018-12-29 18:03:55 -0500

    firebase admin added to functions/index

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -14,7 +16,7 @@ const app = require('express')();
 const set = require('./set');
 
 
-const port = 300;
+// const port = 300;
 
 app.get('/', (req, res) => {
     console.log(`user is here`);
@@ -52,7 +54,22 @@ app.get('/display', (req, res) => {
     res.json(set.display());
 });
 
-app.listen(port, e => {
+
+/*
+// Take the text parameter passed to this HTTP endpoint and insert it into the
+// Realtime Database under the path /messages/:pushId/original
+exports.addMessage = functions.https.onRequest((req, res) => {
+    // Grab the text parameter.
+    const original = req.query.text;
+    // Push the new message into the Realtime Database using the Firebase Admin SDK.
+    return admin.database().ref('/messages').push({original: original}).then((snapshot) => {
+      // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+      return res.redirect(303, snapshot.ref.toString());
+    });
+  });
+
+  */
+app.listen(300, e => {
     console.log(`Listening on port ${port}`)
 });
 
